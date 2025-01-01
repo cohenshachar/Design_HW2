60808b69a3a05d7704596515c86839c67fc8df39
o.mahmud <O.mahmud@campus.technion.ac.il>
2025-01-01 18:03:04 +0200

    added line storage , sotrable and storagedummy

---
+++ .gradle/8.4/dependencies-accessors/31143beffb2c038fd1247d4b0b257b6d765ba322/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,145 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final KotlinLibraryAccessors laccForKotlinLibraryAccessors = new KotlinLibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+    /**
+     * Returns the group of libraries at kotlin
+     */
+    public KotlinLibraryAccessors getKotlin() {
+        return laccForKotlinLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Returns the group of bundles at bundles
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class KotlinLibraryAccessors extends SubDependencyFactory {
+        private final KotlinGradleLibraryAccessors laccForKotlinGradleLibraryAccessors = new KotlinGradleLibraryAccessors(owner);
+
+        public KotlinLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Returns the group of libraries at kotlin.gradle
+         */
+        public KotlinGradleLibraryAccessors getGradle() {
+            return laccForKotlinGradleLibraryAccessors;
+        }
+
+    }
+
+    public static class KotlinGradleLibraryAccessors extends SubDependencyFactory {
+
+        public KotlinGradleLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for plugin (org.jetbrains.kotlin:kotlin-gradle-plugin)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getPlugin() {
+                return create("kotlin.gradle.plugin");
+        }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        private final KotlinVersionAccessors vaccForKotlinVersionAccessors = new KotlinVersionAccessors(providers, config);
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Returns the group of versions at versions.kotlin
+         */
+        public KotlinVersionAccessors getKotlin() {
+            return vaccForKotlinVersionAccessors;
+        }
+
+    }
+
+    public static class KotlinVersionAccessors extends VersionFactory  {
+
+        private final KotlinGradleVersionAccessors vaccForKotlinGradleVersionAccessors = new KotlinGradleVersionAccessors(providers, config);
+        public KotlinVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Returns the group of versions at versions.kotlin.gradle
+         */
+        public KotlinGradleVersionAccessors getGradle() {
+            return vaccForKotlinGradleVersionAccessors;
+        }
+
+    }
+
+    public static class KotlinGradleVersionAccessors extends VersionFactory  {
+
+        public KotlinGradleVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: kotlin.gradle.plugin (2.0.20)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getPlugin() { return getVersion("kotlin.gradle.plugin"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+}
